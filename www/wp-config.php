7c598388422a420756add6120cd928367d90a6c7
Jesse Kade <jessekade@gmail.com>
2011-03-03 22:31:56 -0800

    final revisions

--- www/wp-config.php
+++ www/wp-config.php
@@ -22,10 +22,10 @@ define('DB_NAME', 'aho-blog2');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:8889');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
--- www/wp-content/themes/abundant-harvest/page.php
+++ www/wp-content/themes/abundant-harvest/page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
+<div class="post">
 			<?php get_template_part( 'loop', 'page' ); ?>
-			
+</div>			
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
